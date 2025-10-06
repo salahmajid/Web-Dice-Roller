6729e92fadef72f32403bc8a2feaa08b9c1e3a89
gitsalah <business.salahabdelmajid@gmail.com>
2025-10-05 22:04:36 -0500

    made changes to exisiting code to adjust for implementation of node.js

--- dice-roller.js
+++ dice-roller.js
@@ -1,25 +1,35 @@
-function rollDice() {
-    for (let i=1; i<= 5; i++) {
-        const dieValue = Math.floor(Math.random() * 6) + 1;
-        document.getElementById(`die${i}`).value = dieValue;
+const API_URL = 'dice-server-sja-awf9gzgearfag4eg.canadacentral-01.azurewebsites.net';
+
+async function rollDice() {
+    try {
+        const response = await fetch(`${API_URL}/api/roll`);
+        const data = await response.json();
+
+        for (let i = 1; i <= 5; i++) {
+            document.getElementById(`die${i}`).value = data.dice[i-1];
+        }
+    }
+    catch (error) {
+        alert('Server error: make sure server is running');
     }
 }
 
-window.onload = function() {
+
+window.onload = function () {
+    fetch(`${API_URL}/api/wake`).catch(e => console.log('Wake failed'));
+
+
     rollDice();
-
     const rollButton = document.getElementById('rollButton');
-
     if (rollButton) {
         rollButton.addEventListener('click', rollDice);
-
         rollButton.focus();
     }
 
-    this.document.addEventListener('keypress', function(event){
-        if (event.key === 'enter'){
+    document.addEventListener('keypress', function (event) {
+        if (event.key === 'enter') {
             const button = document.getElementById('rollButton');
-            if(button){
+            if (button) {
                 button.click();
             }
         }
@@ -27,3 +37,7 @@ window.onload = function() {
 
 
 };
+
+function testCORS() {
+    fetch(`${API_URL}/api/no-cors`).catch(e => alert('CORS blocked'));
+}
